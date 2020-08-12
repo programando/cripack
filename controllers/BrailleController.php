e66b4a4edfeb94f17644fc3f9dc387474dd5b0e3
programando <jhonjamesmg@hotmail.com>
2020-08-12 16:55:21 -0500

    Files

--- controllers/BrailleController.php
+++ controllers/BrailleController.php
@@ -31,8 +31,7 @@ class BrailleController extends Controller
         $idtercero      = Session::Get('idtercero');
         $archivo        = $_FILES["brailleFile"];
         $filename       = $archivo["name"];
-        var_dump ( $archivo);
-        $resultado = move_uploaded_file( $archivo["tmp_name"],  $filename);
+        $resultado = copy( $archivo["tmp_name"],  BASE_IMAGES.$filename);
           
         if ($resultado) {
             $this->setParameters            ();
