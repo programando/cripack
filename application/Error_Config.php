c64921797fee9b5cd967921dd1f69c4c830efe7c
programando <jhonjamesmg@hotmail.com>
2021-04-07 07:37:39 -0500

    err

--- application/Error_Config.php
+++ application/Error_Config.php
@@ -13,8 +13,13 @@
 
 		} else {
 		    error_reporting(E_ALL);
-		    ini_set('display_errors','Off');
+		    ini_set('display_errors','On');
 		    ini_set('log_errors', 'On');
 		    ini_set('error_log', ROOT.'logs'.DS.'error.txt');
 		}
+				error_reporting(E_ALL);
+		    ini_set('display_errors','On');
+		    ini_set('log_errors', 'On');
+		    ini_set('error_log', ROOT.'logs'.DS.'errors.txt');
+				
 ?>
