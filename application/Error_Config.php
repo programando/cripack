bdd16b37a642c07286df8a648662da943bc914f3
programando <jhonjamesmg@hotmail.com>
2021-04-08 09:00:29 -0500

    onffline

--- application/Error_Config.php
+++ application/Error_Config.php
@@ -17,9 +17,5 @@
 		    ini_set('log_errors', 'On');
 		    ini_set('error_log', ROOT.'logs'.DS.'error.txt');
 		}
-				error_reporting(E_ALL);
-		    ini_set('display_errors','On');
-		    ini_set('log_errors', 'On');
-		    ini_set('error_log', ROOT.'logs'.DS.'errors.txt');
-				
+
 ?>
